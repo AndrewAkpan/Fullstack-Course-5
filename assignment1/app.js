eda341dd59141db724e21b54f9058e318a4ed907
AndrewAkpan <aakpan96@gmail.com>
2017-07-05 01:10:29 -0400

    Update app.js

--- assignment1/app.js
+++ assignment1/app.js
@@ -9,15 +9,16 @@ function LunchCheckController($scope){
      $scope.message = "";
      $scope.displayMsg = function (){
         if ( $scope.foodlist === "" ){
-            return $scope.message = "Please enter data first";
+            $scope.message = "Please enter data first";
         }
         var f_list= $scope.foodlist.split(",");
         if ( f_list.length <= 3 && f_list.length > 0){
-          return $scope.message = "Enjoy!";
+          $scope.message = "Enjoy!";
         }
         if ( f_list.length >3){
-          return $scope.message = "Too much!";
+          $scope.message = "Too much!";
         }
+       return $scope.message;
     };
 }
 
