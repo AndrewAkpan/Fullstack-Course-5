3bd8c877c6e0c2ee967195cfd78e5ce0424141af
AndrewAkpan <aakpan96@gmail.com>
2017-07-05 02:30:38 -0400

    Update app.js

--- assignment1/app.js
+++ assignment1/app.js
@@ -4,26 +4,41 @@
   angular.module('TooMuch', [])
   .controller('TooMuchController', TooMuchController);
 
-LunchCheckController.$inject = ['$scope'];
+TooMuchController.$inject = ['$scope'];
 function TooMuchController($scope)
 {
      $scope.message = "Enter data";
      $scope.CheckMuch = function ()
       {
         var input= $scope.userInput.split(",");
+        var length = input.length;
+
+        length= CheckSpace(input,length);
 
         if ( $scope.userInput === "" || $scope.userInput === " "  ){
             return $scope.message = "Please enter data first";
         }
         
-        if ( input.length <= 3 && input.length > 0){
+        if ( length <= 3 && length > 0){
           return $scope.message = "Enjoy!";
         }
-        if ( input.length >3){
+        if ( length >3){
           return $scope.message = "Too much!";
         }
        
      };
+
+     function CheckSpace(array,int)
+     {
+      var int = array.length;
+
+      for (var i = int - 1; i >= 0; i--) 
+       {
+        if (array[i]=="" || array[i]==" "|| array[i]=="  "|| array[i]=="   "|| array[i]=="    "|| array[i]=="     "|| array[i]=="      ")
+          int=int-1;
+       }
+       return int;
+     }
 }
 
 })();
